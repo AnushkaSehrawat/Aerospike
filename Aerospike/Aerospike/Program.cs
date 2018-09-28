94fefc962464a5419d2bd38e798d56c42f96b187
Anushka Sehrawat <asehrawat@tavisca.com>
2018-09-28 18:45:21 +0530

    Fetching records from csv file and storing in aerospike

---
+++ Aerospike/Aerospike/Program.cs
@@ -0,0 +1,51 @@
+﻿using Aerospike.Client;
+using CsvHelper;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerospike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:/Users/asehrawat/Desktop/2018-01-trump-twitter-wars/data/tweets/tweets1.csv");
+            CsvReader cr = new CsvReader(sr);
+            IEnumerable<Tweet> record = cr.GetRecords<Tweet>();
+            var client = new AerospikeClient("18.235.70.103", 3000);
+            string nameSpace = "AirEngine";
+            string setName = "Anushka";
+            foreach (var row in record)
+            {
+                    var key = new Key(nameSpace, setName, row.id);
+                    client.Put(new WritePolicy(), key, new Bin[]
+                    {
+                        new Bin("text", row.text),
+                        new Bin("favorited", row.favorited),
+                        new Bin("replyToSN", row.replyToSN),
+                        new Bin("created", row.created),
+                        new Bin("truncated", row.truncated),
+                        new Bin("replyToSID", row.replyToSID),
+                        new Bin("id", row.id),
+                        new Bin("replyToUID", row.replyToUID),
+                        new Bin("StatusSource", row.statusSource),
+                        new Bin("screenName", row.screenName),
+                        new Bin("retweetCount", row.retweetCount),
+                        new Bin("isRetweet", row.isRetweet),
+                        new Bin("retweeted", row.retweeted),
+                        new Bin("screenName", row.screenName),
+                        new Bin("timestamp", row.timestamp),
+                        new Bin("us_timestamp", row.us_timestamp),
+                        new Bin("date", row.date)
+
+
+                    });
+            }
+        }
+    }
+}
---
+++ Aerospike/Aerospike/Tweet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerospike
+{
+    class Tweet
+    {
+        public string text { get; set; }
+        public string favorited { get; set; }
+
+        public string favoriteCount { get; set; }
+        public string replyToSN { get; set; }
+        public string created { get; set; }
+
+        public string truncated { get; set; }
+
+        public string replyToSID { get; set; }
+
+        public string id { get; set; }
+        public string replyToUID { get; set; }
+
+        public string statusSource { get; set; }
+        public string screenName { get; set; }
+
+        public string retweetCount { get; set; }
+
+        public string isRetweet { get; set; }
+
+        public string retweeted { get; set; }
+        public string timestamp { get; set; }
+        public string us_timestamp { get; set; }
+
+        public string date { get; set; }
+    }
+}
